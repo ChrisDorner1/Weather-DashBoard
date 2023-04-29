018d85f1c35dc04afea75c7b3563f3606070a443
Chris Spivey <theori373@gmail.com>
2023-04-29 14:43:27 -0700

    future weather api functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var searchBar = document.querySelector(".searchBar")
 var searchBtn = document.querySelector(".searchBtn")
-var today = document.querySelector(".current")
+var tomorrow = document.querySelector(".tomorrow")
+var currentT = document.querySelector(".currentT")
+var nextT = document.querySelector(".nextT")
+var nextT2 = document.querySelector(".nextT2")
+var nextT3 = document.querySelector(".nextT3")
+var nextT4 = document.querySelector(".nextT4")
 
 searchBtn.addEventListener("click", function(){
     input()
@@ -11,7 +16,7 @@ function input() {
     if (cityName == "") {
         alert("Please enter the city you want to see.")
     } else {
-        // today.style.display = "flex"
+        tomorrow.style.display = "block"
         // today.textContent = cityName
         var url= "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=&appid=e19e2b263fc9d0529488931d1e9bdaf5"
         fetch(url)
@@ -29,7 +34,13 @@ function input() {
                 return response.json()
             })
             .then(function(data){
-                console.log(data)
+                console.log(data.list)
+                console.log(data.list[0].weather[0].main)
+                currentT.textContent = data.list[0].main.temp + " Degrees F " + data.list[0].weather[0].main
+                nextT.textContent = data.list[8].main.temp + " Degrees F " + data.list[8].weather[0].main
+                nextT2.textContent = data.list[16].main.temp + " Degrees F " + data.list[16].weather[0].main
+                nextT3.textContent = data.list[24].main.temp + " Degrees F " + data.list[24].weather[0].main
+                nextT4.textContent = data.list[32].main.temp + " Degrees F " + data.list[32].weather[0].main
             })
         })
         
